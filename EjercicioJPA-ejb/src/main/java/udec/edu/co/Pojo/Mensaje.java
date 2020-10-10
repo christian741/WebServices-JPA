58db76888876536aac49f0800fb53889b8faf277
christian741 <christian.xd.417@gmail.com>
2020-10-10 10:21:56 -0500

    Creacion Pojos o Models de respuesta servicios

---
+++ EjercicioJPA-ejb/src/main/java/udec/edu/co/Pojo/Mensaje.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udec.edu.co.Pojo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Christian
+ */
+public class Mensaje {
+    
+    private ErrorWraper error;
+    private ArrayList<Object> objeto;
+
+    public Mensaje(){
+        
+    }
+    
+    public Mensaje(ErrorWraper error, ArrayList<Object> objeto) {
+        this.error = error;
+        this.objeto = objeto;
+    }
+
+    public ErrorWraper getError() {
+        return error;
+    }
+
+    public void setError(ErrorWraper error) {
+        this.error = error;
+    }
+
+    public ArrayList<Object> getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(ArrayList<Object> objeto) {
+        this.objeto = objeto;
+    }
+    
+    
+    
+}
